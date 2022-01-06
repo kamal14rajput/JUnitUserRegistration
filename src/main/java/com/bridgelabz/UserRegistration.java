f9a7ef0d002775c03df006dd1e93b03dbeb2fd34
kamal14rajput <kamal14rajput@gmail.com>
2022-01-06 21:23:51 +0530

    UPDATED: last name validated

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,11 +5,21 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	private static final String FirstName = "^[A-Z][a-z]{2,}";
+	private static final String LastName = "^[A-Z][a-z]{2,}";
 
 	public boolean userFirstName(String firstName) {
 
 		Pattern pattern = Pattern.compile(FirstName);
 		Matcher matcher = pattern.matcher(firstName);
+
+		return matcher.matches();
+
+	}
+
+	public static boolean userLastName(String lastName) {
+
+		Pattern pattern = Pattern.compile(LastName);
+		Matcher matcher = pattern.matcher(lastName);
 
 		return matcher.matches();
 
